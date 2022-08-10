4fbe54dae6e476ae78caf428e4267009e9ed5709
David Johnston <david.johnston@verint.com>
2022-08-10 23:45:07 +0100

    Add notes API

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+[
++amplify/backend/api/notesapp/schema.graphql,b\7\b72a18a5adc299b7c8d2cb6fe12a540f7583f33a
--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
-import logo from './logo.svg';
-import "@aws-amplify/ui-react/styles.css";
 import './App.css';
-import {Button, Card, Heading, Image, View, withAuthenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import {Authenticator} from "@aws-amplify/ui-react";
+import {useEffect, useState} from "react";
+import {API} from "aws-amplify";
+import {createNote as createNoteMutation, deleteNote as deleteNoteMutation} from './graphql/mutations'
+import {listNotes} from "./graphql/queries";
 
-const App = ({signOut}) => (
-    <View className={"App"}>
-        <Card>
-            <Image alt={"logo"} src={logo} className={"App-logo"}/>
-            <Heading level={1}>We now have Auth!</Heading>
-        </Card>
-        <Button onClick={signOut}>Sign Out</Button>
-    </View>
-);
+const initialFormState = {name: '', description: ''};
 
-export default withAuthenticator(App);
+const App = () => {
+    const [formData, setFormData] = useState(initialFormState);
+    const [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    async function fetchNotes() {
+        const apiData = await API.graphql({query: listNotes});
+        setNotes(apiData.data.listNotes.items);
+    }
+
+    async function createNote() {
+        if (!formData.name || !formData.description) return;
+        await API.graphql({query: createNoteMutation, variables: {input: formData}});
+        setNotes([...notes, formData]);
+        setFormData(initialFormState);
+    }
+
+    async function deleteNote({id}) {
+        const newNotesArray = notes.filter(note => note.id !== id);
+        setNotes(newNotesArray);
+        await API.graphql({query: deleteNoteMutation, variables: {input: {id}}});
+    }
+
+    return (<Authenticator>
+        {({signOut, user}) => (<div className={"App"}>
+            <h1>My Notes App {user.attributes.email}/{user.username}</h1>
+            <input
+                onChange={e => setFormData({...formData, 'name': e.target.value})}
+                placeholder="Note name"
+                value={formData.name}
+            />
+            <input
+                onChange={e => setFormData({...formData, 'description': e.target.value})}
+                placeholder="Note description"
+                value={formData.description}
+            />
+            <button onClick={createNote}>Create Note</button>
+            <div style={{marginBottom: 30}}>
+                {notes.map(note => (<div key={note.id || note.name}>
+                    <h2>{note.name}</h2>
+                    <p>{note.description}</p>
+                    <button onClick={() => deleteNote(note)}>Delete note</button>
+                </div>))}
+            </div>
+            <button onClick={signOut}>Sign out</button>
+        </div>)}
+    </Authenticator>)
+};
+
+export default App;
