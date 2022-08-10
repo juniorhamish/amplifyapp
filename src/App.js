aa083f105878bb8619c8301e8a2524cf7a2f7298
David Johnston <david.johnston@verint.com>
2022-08-10 22:34:39 +0100

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
+import "@aws-amplify/ui-react/styles.css";
 import './App.css';
+import {Button, Card, Heading, Image, View, withAuthenticator} from "@aws-amplify/ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
-}
+const App = ({signOut}) => (
+    <View className={"App"}>
+        <Card>
+            <Image alt={"logo"} src={logo} className={"App-logo"}/>
+            <Heading level={1}>We now have Auth!></Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+    </View>
+);
 
-export default App;
+export default withAuthenticator(App);
